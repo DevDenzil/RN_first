171d7c2f5bb2813cf4a81da315f52ade76cc539b
Denys <denzil92@gmail.com>
2023-10-31 14:37:49 +0100

    ss

--- src/screens/ColoredSquareScreen.js
+++ src/screens/ColoredSquareScreen.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Text, StyleSheet, Button, View, TextInput } from 'react-native';
 
 const COLOR_INCREMENT = 15;
 
-export const ColoredSquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
+const reducer = (state, action) => {
+  // state === {red: number, green: number, blue:number}
+  // action === {colorToChange: 'red' || 'green' || 'blue', amount: number}
+  switch (action.colorToChange) {
+    case 'red':
+      return { ...state, red: state.red + action.amount };
+    case 'green':
+      return { ...state, green: state.green + action.amount };
+    case 'blue':
+      return { ...state, blue: state.blue + action.amount };
+    default:
+      return state;
+  }
+};
 
-  const setColor = (color, n) => {
-    return -1 < color + n && color + n < 256 ? color + n : color;
-  };
+export const ColoredSquareScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
 
   const setRandomColor = () => {
     setRed(Math.round(Math.random() * 255));
@@ -22,21 +31,11 @@ export const ColoredSquareScreen = () => {
     const colorText = color.toString();
     return (
       <View>
-        <Text style={styles.text}>{colorTitle}: {color}</Text>
-        <TextInput
-          style={styles.text}
-          value={colorText}
-          inputMode="numeric"
-          onChangeText={(e) => setter(Number(e))}
-        />
-        <Button
-          onPress={() => setter(setColor(color, COLOR_INCREMENT))}
-          title={`More ${colorTitle}`}
-        />
-        <Button
-          onPress={() => setter(setColor(color, -1 * COLOR_INCREMENT))}
-          title={`Less ${colorTitle}`}
-        />
+        <Text style={styles.text}>
+          {colorTitle}: {color}
+        </Text>
+        <Button onPress={() => console.log(1)} title={`More ${colorTitle}`} />
+        <Button onPress={() => console.log(1)} title={`Less ${colorTitle}`} />
       </View>
     );
   };
@@ -45,9 +44,9 @@ export const ColoredSquareScreen = () => {
     <View>
       <Text style={{ fontSize: 30 }}>Colored Square</Text>
       <Button onPress={() => setRandomColor()} title="Set Random color" />
-      <OneColor colorTitle="Red" setter={setRed} color={red} />
-      <OneColor colorTitle="Green" setter={setGreen} color={green} />
-      <OneColor colorTitle="Blue" setter={setBlue} color={blue} />
+      <OneColor colorTitle="Red" />
+      <OneColor colorTitle="Green" />
+      <OneColor colorTitle="Blue" />
       <View
         style={{
           height: 200,
